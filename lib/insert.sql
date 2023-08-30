8d6e0936768e54ec432a7d09fa809ee00c807211
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-08-30 20:48:48 +0300

    BEARS

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',7,'M','Brown','cool',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Rowdy',9,'M','Brown','furious',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Tabitha',5,'F','Black','very furious',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',15,'M','Brown','calm',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Melissa',9,'F','Brown','moderate',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Grinch',20,'M','Black','moderate',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Wendy',17,'F','Black','calm',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES (NULL,1,'M','Black','playful',1);
